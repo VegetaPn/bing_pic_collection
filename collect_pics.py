3429709e4a0f1122149f91620872bef44745988d
延昊南 <yhnkb@hotmail.com>
2016-04-17 02:45:59 +0800

    初始化，必应图片下载

---
+++ collect_pics.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+import urllib
+import datetime
+
+COLLECTION_DIR = 'collection/images/'
+
+
+pic_url = None
+f = urllib.urlopen('http://cn.bing.com/')
+page_source = str(f.read())
+
+if page_source.find('http://s.cn.bing.net/') >= 1:
+	pre_url = page_source.split('http://s.cn.bing.net')[1]
+	clean_url = pre_url.split('.jpg')[0]
+	pic_url = 'http://s.cn.bing.net%s.jpg' % clean_url
+	print(pic_url)
+
+if pic_url is not None:
+	nowa_time = datetime.datetime.now().strftime('%Y%m%d')
+	urllib.urlretrieve(pic_url, COLLECTION_DIR+nowa_time+'.jpg')
+	print('download successful')
