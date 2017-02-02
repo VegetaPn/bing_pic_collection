159f79bb9d6dfdbe8b1467208a2394f11a4bb015
延昊南 <yhnkb@hotmail.com>
2017-02-02 13:50:27 +0800

    update

--- collect_pics.py
+++ collect_pics.py
@@ -11,11 +11,15 @@ def collect_bing_picure():
 	pic_url = None
 	f = urllib.urlopen('http://cn.bing.com/')
 	page_source = str(f.read())
+	f.close()
 
 	if page_source.find('s.cn.bing.net') >= 1:
 		pre_url = page_source.split('s.cn.bing.net')[1]
 		clean_url = pre_url.split('.jpg')[0].replace('\\', '')
 		pic_url = 'http://s.cn.bing.net%s.jpg' % clean_url
+		print(pic_url)
+	elif page_source.find('g_img={url: "') >= 1:
+		pic_url = 'http://cn.bing.com' + page_source.split('g_img={url: "')[1].split('"')[0]
 		print(pic_url)
 
 	if pic_url is not None:
